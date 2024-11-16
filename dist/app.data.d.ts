d4ccf2d5d277212f3b8e3e2c7694024c01bc112d
Mohamad Hanifan <mohamad.hanifan@efishery.com>
2024-11-16 23:24:32 +0700

    feat: remove untracked files

--- dist/app.data.d.ts
+++
@@ -1,8 +0,0 @@
-import { Author, Book, Tag } from './app.entity';
-interface Data {
-    authors: Author[];
-    tags: Tag[];
-    books: Book[];
-}
-declare const data: Data;
-export default data;
--- dist/app.helper.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function getByPagination<T>(data: T[], page: number, size: number): T[];
-export declare function getFiltered<T>(array: T[], search: string, keys: (keyof T)[]): T[];
